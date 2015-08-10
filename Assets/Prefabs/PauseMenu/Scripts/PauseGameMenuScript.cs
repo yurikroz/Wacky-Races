f2f08cb7ea96efb46e2c1a0425f15e4aefc3d85a
Yuri Kroz <splanger@gmail.com>
2015-08-11 01:29:05 +0300

    Turned Pause Menu into a prefab

---
+++ Assets/Prefabs/PauseMenu/Scripts/PauseGameMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseGameMenuScript : MonoBehaviour
+{
+
+    public Canvas pauseMenu;
+
+    public void Start()
+    {
+        pauseMenu = pauseMenu.GetComponent<Canvas>();
+        pauseMenu.enabled = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            pauseMenu.enabled = !pauseMenu.enabled;
+            //menu.SetActive(isShowing);
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void Cancel()
+    {
+        pauseMenu.enabled = false;
+    }
+}
