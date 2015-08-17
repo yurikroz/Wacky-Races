e89193390badd1e366917f9eb9ec8cacbb68500e
Yuri Kroz <splanger@gmail.com>
2015-08-17 23:05:43 +0300

    Added properties for booster + Added "following camera" script

--- Assets/Prefabs/Booster/CarBoost.cs
+++ Assets/Prefabs/Booster/CarBoost.cs
@@ -4,43 +4,39 @@ using UnityStandardAssets.Vehicles.Car;
 
 public class CarBoost : MonoBehaviour {
 
-    //public GameObject playerCar;
-    //float start;
 	// Use this for initialization
+    public float boostMagnitude = 0.0f;
+    public float boostEffectLength = 1.0f;
+    public bool active = true;
+
 	void Start () {
+        Debug.Log(this.gameObject.tag + " has been initialized!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Car") || other.gameObject.CompareTag("Player"))
+        if ((other.gameObject.CompareTag("Car") || other.gameObject.CompareTag("Player")) && active)
         {
-            //other.gameObject.SetActive(false);
+            active = false;
             StartCoroutine(BoostCar(other.gameObject));
-            this.gameObject.SetActive(false);
+            MeshRenderer render = this.gameObject.GetComponentInChildren<MeshRenderer>();
+            render.enabled = false;
         }
-        //Destroy(other.gameObject);
     }
 
     public IEnumerator BoostCar(GameObject playerCar) 
     {
         // Increase a player's car speed
-        playerCar.GetComponent<CarController>().CurrentSpeed = 20.0f;
+        playerCar.GetComponent<CarController>().CurrentSpeed = boostMagnitude;
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(boostEffectLength);
 
-        playerCar.GetComponent<CarController>().CurrentSpeed = -20.0f;
+        playerCar.GetComponent<CarController>().CurrentSpeed = -boostMagnitude;
 
-        // Decrease a player's car speed
-        /*playerCar.GetComponent<CarController>().CurrentSpeed -= 10.0f;
-        yield return new WaitForSeconds(0.2f);
-        playerCar.GetComponent<CarController>().CurrentSpeed -= 10.0f;
-        yield return new WaitForSeconds(0.2f);
-        playerCar.GetComponent<CarController>().CurrentSpeed -= 10.0f;*/
-        Debug.Log("in boostCar");
+        this.gameObject.SetActive(false);
     }
 }
