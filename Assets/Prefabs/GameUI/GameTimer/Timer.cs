c1ab5b77ede73ae8ec8bdfa7d3fa3280dbf610f2
Yuri Kroz <splanger@gmail.com>
2015-08-19 21:30:07 +0300

    Implemented a working GameTimer

---
+++ Assets/Prefabs/GameUI/GameTimer/Timer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public float timePassed = 0f;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timePassed += Time.deltaTime;
+        updateTimerText();
+	}
+
+    private void updateTimerText()
+    {
+        float minutes = Mathf.Floor(timePassed / 60);
+        float seconds = Mathf.RoundToInt(timePassed) % 60;
+        float milliseconds = Mathf.RoundToInt(Mathf.Abs(timePassed - seconds) * 100);
+
+        if (minutes < 10)
+        {
+            timerText.text = "0" + minutes.ToString() + " : ";
+        }
+        else
+        {
+            timerText.text = minutes.ToString() + " : ";
+        }
+
+        if (seconds < 10)
+        {
+            timerText.text += "0" + seconds.ToString() + ".";
+        }
+        else
+        {
+            timerText.text += seconds.ToString() + ".";
+        }
+
+        if (milliseconds < 10)
+        {
+            timerText.text += "0" + milliseconds.ToString();
+        } else {
+            timerText.text += milliseconds.ToString();
+        }
+    }
+}
